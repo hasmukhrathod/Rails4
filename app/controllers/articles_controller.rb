d3238de0d1349715f5d7133ba05c682edeefe001
Hasmukh <hasmukh@hasmukh-Inspiron-1440.(none)>
2014-04-07 07:35:35 +0530

    CRUD Operation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,40 @@
 class ArticlesController < ApplicationController
 
+  include ArticlesHelper
+
 	def index
-		#We want the list of articles to be accessible from both the controller and the view that we’re about to create.
+		# We want the list of articles to be accessible from both the controller and the view that we’re about to create.
 		# If we had just named it articles, that local variable would only be available within the index method of the controller. 
   		@articles = Article.all
 	end
+
 	def show
 		@article = Article.find(params[:id])
 	end
 
-		
+	def new
+		@article = Article.new
+	end
+	
+	def create
+	  	 @article = Article.new(article_params)
+  		 @article.save
+  		 redirect_to article_path(@article)
+	end
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+	end
+	def edit
+  		@article = Article.find(params[:id])
+	end
+
+	def update
+  		@article = Article.find(params[:id])
+  		@article.update(article_params)	
+		  flash.notice = "Article '#{@article.title}' Updated!"
+		redirect_to article_path(@article)
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
 
 Blogger::Application.routes.draw do
 	#This line tells Rails that we have a resource named articles.
+	root to: 'articles#index'
 	resources :articles
 end
